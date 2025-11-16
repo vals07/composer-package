45ac8052c8b20f9cb9801b5d5d8c8adceeebc789
buglova <buglova@ujin.tech>
2025-11-16 13:55:17 +0300

    добавлена поддержка мультибайтовых строк

--- src/StringProcessor.php
+++ src/StringProcessor.php
@@ -11,9 +11,23 @@ class StringProcessor {
      */
     public function isPalindrome(string $input): bool {
         // Приводим к нижнему регистру для регистронезависимого сравнения
-        $normalized = strtolower($input);
+        $normalized = mb_strtolower($input);
 
         // Сравниваем строку с перевернутой версией
-        return $normalized === strrev($normalized);
+        return $normalized === $this->mb_strrev($normalized);
+    }
+
+    /**
+     * Переворачивает мультибайтовую строку
+     *
+     * @param string $string Строка, которую переворачиваем
+     * @param string|null $encoding Кодировка символов. Если не указано, будет использована внутренняя кодировка символов файла скрипта.
+     * @return string Перевернутая строка
+     */
+    private function mb_strrev(string $string, ?string $encoding = null): string
+    {
+        $chars = mb_str_split($string, 1, $encoding ?: mb_internal_encoding());
+        
+        return implode('', array_reverse($chars));
     }
 }
